fe6981b9695dc2c8d38049b41bedfc74cd8cd91c
Elena Sufieva <fiolent06@gmail.com>
2018-06-12 17:38:04 +0300

    Save several images as assets for demo

---
+++ src/assets/images/images.ts
@@ -0,0 +1,7 @@
+export const blueberryCakes = require('./blueberry-cakes.jpg');
+
+export const chocolateCupcakes = require('./chocolate-cupcakes.jpg');
+
+export const starCake = require('./star-cake.jpg');
+
+export const milleFeuille = require('./mille-feuille.jpg');
